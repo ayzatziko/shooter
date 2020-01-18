642c2d77a4c9b48baf1b92756aa75b1b722c99b0
Ayzat Sadykov <ayzat.ziko.93@gmail.com>
2020-01-19 00:57:34 +0300

    added images and animation

--- game.py
+++ game.py
@@ -5,8 +5,22 @@ caption = "Shooter"
 displayW = 500
 displayH = 500
 fps = 27
-bg = (0, 0, 0)
-world = {'run': True, 'rect': (50, 400, 40, 60), 'color': (255, 0, 0), 'vel': 5, 'isJump': False, 'jumpHeight': 10, 'jumpCount': 5}
+world = {
+    'run': True,
+    'vel': 5,
+    'cx': 30, 'cy': 400, 'cw': 64, 'ch': 64,
+    'isJump': False,
+    'jumpHeight': 10,
+    'jumpCount': 5,
+    'walkCount': 0,
+    'char_dir': 0,
+}
+
+# images
+walkRight = [pygame.image.load('R1.png'),pygame.image.load('R2.png'),pygame.image.load('R3.png'),pygame.image.load('R4.png'),pygame.image.load('R5.png'),pygame.image.load('R6.png'),pygame.image.load('R7.png'),pygame.image.load('R8.png'),pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'),pygame.image.load('L2.png'),pygame.image.load('L3.png'),pygame.image.load('L4.png'),pygame.image.load('L5.png'),pygame.image.load('L6.png'),pygame.image.load('L7.png'),pygame.image.load('L8.png'),pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+stand = pygame.image.load('standing.png')
 
 win = pygame.display.set_mode((displayW, displayH))
 pygame.display.set_caption(caption)
@@ -18,15 +32,19 @@ def handle_events(w):
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        if w['rect'][0] <= w['vel']:
+        world['walkCount'] += 1
+        world['char_dir'] = -1
+        if w['cx'] <= w['vel']:
             pass
         else:
-            w['rect'] = (w['rect'][0] - w['vel'], w['rect'][1], w['rect'][2], w['rect'][3])
+            w['cx'] = w['cx'] - w['vel']
     elif keys[pygame.K_RIGHT]:
-        if w['rect'][0] + w['rect'][2] >= displayW - w['vel']:
+        world['walkCount'] += 1
+        world['char_dir'] = 1
+        if w['cx'] + w['cw'] >= displayW - w['vel']:
             pass
         else:
-            w['rect'] = (w['rect'][0] + w['vel'], w['rect'][1], w['rect'][2], w['rect'][3])
+            w['cx'] = w['cx'] + w['vel']
 
     if not w['isJump']:
         if keys[pygame.K_UP]:
@@ -43,13 +61,24 @@ def handle_events(w):
             w['isJump'] = False
 
         h = h ** 2 * d / 2
-        w['rect'] = (w['rect'][0], w['rect'][1] - h, w['rect'][2], w['rect'][3])
+        w['cy'] = w['cy'] - h
 
     return w
 
 def draw():
-    win.fill(bg)
-    pygame.draw.rect(win, world['color'], world['rect'])
+    win.blit(bg, (0, 0))
+
+    if world['walkCount'] >= 27:
+        world['walkCount'] = 0
+
+    i = world['walkCount'] // 3
+    if world['char_dir'] == 0: # standing
+        win.blit(stand, (world['cx'], world['cy']))
+    if world['char_dir'] == 1: # going right
+        win.blit(walkRight[i], (world['cx'], world['cy']))
+    if world['char_dir'] == -1: # going left
+        win.blit(walkLeft[i], (world['cx'], world['cy']))
+
     pygame.display.update()
 
 while world['run']:
